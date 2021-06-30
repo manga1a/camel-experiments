de3a67359d189185e6385d53a98a462ba4375dbf
Mangala <m@mangala.dev>
2021-06-30 20:21:57 +1000

    first unit test

---
+++ FileCopier/src/test/java/examples/FirstTest.java
@@ -0,0 +1,33 @@
+package examples;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Route;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.io.File;
+
+public class FirstTest extends CamelTestSupport {
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file://target/inbox")
+                        .to("file://target/outbox");
+            }
+        };
+    }
+
+    @Test
+    public void testMoveFile() throws Exception {
+        template.sendBodyAndHeader("file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
+
+        Thread.sleep(2000);
+
+        File target = new File("target/outbox/hello.txt");
+        assertTrue("File not moved", target.exists());
+    }
+}
