980a47c1f8caa84997d4b94384d4f992ec353643
Mangala <m@mangala.dev>
2021-06-30 21:00:35 +1000

    Replace thread sleep with NotifyBuilder

--- FileCopier/src/test/java/examples/FirstTest.java
+++ FileCopier/src/test/java/examples/FirstTest.java
@@ -2,6 +2,7 @@ package examples;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.NotifyBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
@@ -9,6 +10,13 @@ import org.junit.Test;
 import java.io.File;
 
 public class FirstTest extends CamelTestSupport {
+
+    public void setUp() throws Exception {
+        deleteDirectory("target/inbox");
+        deleteDirectory("target/outbox");
+        super.setUp();
+    }
+
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
@@ -22,11 +30,17 @@ public class FirstTest extends CamelTestSupport {
 
     @Test
     public void testMoveFile() throws Exception {
+
+        NotifyBuilder notify = new NotifyBuilder(context).whenDone(1).create();
+
         template.sendBodyAndHeader("file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
 
-        Thread.sleep(2000);
+        assertTrue(notify.matchesMockWaitTime());
 
         File target = new File("target/outbox/hello.txt");
         assertTrue("File not moved", target.exists());
+
+        String content = context.getTypeConverter().convertTo(String.class, target);
+        assertEquals("Hello World", content);
     }
 }
