547f54b7449f1eeee47d2562084c63d2ebd926cf
Mangala <m@mangala.dev>
2021-07-01 20:24:15 +1000

    Expectations on message body

--- FileCopier/src/test/java/examples/FirstMockTest.java
+++ FileCopier/src/test/java/examples/FirstMockTest.java
@@ -21,11 +21,17 @@ public class FirstMockTest extends CamelTestSupport {
     @Test
     public void testQuote() throws Exception {
         //Arrange
+        final String body1 = "Camel Rocks!!!";
+        final String body2 = "Hello Camel";
+
         MockEndpoint quote = getMockEndpoint("mock:quote");
-        quote.expectedMessageCount(1);
+        quote.expectedMessageCount(2);
+        //quote.expectedBodiesReceived(body1, body2);
+        quote.expectedBodiesReceivedInAnyOrder(body2, body1);
 
         //Act
-        template.sendBody("stub:jms:topic:quote", "Camel rocks!!!");
+        template.sendBody("stub:jms:topic:quote", body1);
+        template.sendBody("stub:jms:topic:quote", body2);
 
         //Assert
         quote.assertIsSatisfied();
