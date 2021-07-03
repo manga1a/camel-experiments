725d4199be99fc1a67fd0901a1422c7596ecc8b6
Mangala <m@mangala.dev>
2021-07-03 16:54:59 +1000

    Test dynamic route

---
+++ FileCopier/src/test/java/examples/routing/FileToJmsDynamicToTest.java
@@ -0,0 +1,50 @@
+package examples.routing;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import javax.jms.ConnectionFactory;
+
+public class FileToJmsDynamicToTest extends CamelTestSupport {
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext context =  super.createCamelContext();
+
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory =
+                new ActiveMQConnectionFactory("vm://localhost");
+        context.addComponent("jms",
+                JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        return context;
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // load file orders from src/data into the JMS queue
+                from("file:data/inbox?noop=true")
+                        .setHeader("myDest", constant("incomingOrders"))
+                        .toD("jms:${header.myDest}");
+
+                // test that our route is working
+                from("jms:incomingOrders")
+                        .to("mock:incomingOrders");
+            }
+        };
+    }
+
+    @Test
+    public void testPlacingOrders() throws Exception {
+        getMockEndpoint("mock:incomingOrders").expectedMessageCount(1);
+        assertMockEndpointsSatisfied();
+    }
+}
